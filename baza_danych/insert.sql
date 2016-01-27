c364f1028ae2ef275633f1a2ac2082ccdb0c0190
Dominik Adamski <s179647@v-ie.uek.krakow.pl>
2016-01-27 12:35:10 +0100

    Dodanie pliku insert.sql ktory wypelnia tabele utworzone przez baza_danych.sql wartosciami podanymi w pliku - nazwa,opis,cena,zdjecie auta.

---
+++ baza_danych/insert.sql
@@ -0,0 +1,280 @@
+﻿
+INSERT INTO `s179647`.`Category`
+(`category_id`,
+`category`)
+VALUES
+('<{category_id: 1}>',
+'<{category: auta osobowe}>');
+
+INSERT INTO `s179647`.`Category`
+(`category_id`,
+`category`)
+VALUES
+('<{category_id: 2}>',
+'<{category: auta dostawcze}>');
+
+
+INSERT INTO `s179647`.`Category`
+(`category_id`,
+`category`)
+VALUES
+('<{category_id: 3}>',
+'<{category: auta ciężarowe}>');
+
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Ford Mustang',
+300,
+'Ford Mustang – amerykański samochód osobowy typu muscle car produkowany od 1964 roku, jako amerykańska odpowiedź na produkowane w Europie sportowe wozy GT',
+1,
+'http://assets.forddirect.fordvehicles.com/assets/2016_Ford_Mustang_J1/NGBS/Model_Image3/Model_Image3_136B520A-C3FD-6818-DA32-1D68DA321D68.jpg',
+1);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Volkswagen Polo',
+50,
+' samochód osobowy segmentu aut miejskich produkowany przez koncern Volkswagen od 1975 roku. Nazwa Polo pochodzi od nazwy elitarnej dyscypliny sportu.',
+1,
+'https://pl.wikipedia.org/wiki/Volkswagen_Polo#/media/File:VW_Polo_1.2_Tour_%28IV,_Facelift%29_%E2%80%93_Heckansicht,_4._September_2011,_Heiligenhaus.jpg',
+1);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Volkswagen Polo',
+50,
+' samochód osobowy segmentu aut miejskich produkowany przez koncern Volkswagen od 1975 roku. Nazwa Polo pochodzi od nazwy elitarnej dyscypliny sportu.',
+1,
+'https://pl.wikipedia.org/wiki/Volkswagen_Polo#/media/File:VW_Polo_1.2_Tour_%28IV,_Facelift%29_%E2%80%93_Heckansicht,_4._September_2011,_Heiligenhaus.jpg',
+1);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Volkswagen Golf',
+80,
+' Niemiecki samochód klasy kompaktowej, produkowany przez firmę Volkswagen od 1974 roku.',
+1,
+'https://pl.wikipedia.org/wiki/Volkswagen_Golf#/media/File:VW_Golf_1.2_TSI_BlueMotion_Technology_Comfortline_%28VII%29_%E2%80%93_Frontansicht,_4._Januar_2014,_D%C3%BCsseldorf.jpg',
+1);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Fiat Panda ',
+30,
+'Fiat Panda – samochód osobowy klasy aut miejskich produkowany przez włoski koncern FIAT od 1980 roku[1]. Od 2012 roku produkowana jest trzecia generacja modelu.',
+1,
+'https://pl.wikipedia.org/wiki/Fiat_Panda#/media/File:Fiat-Panda_2003.jpg',
+1);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Honda Civic',
+55,
+'Honda Civic - samochód osobowy klasy kompaktowej (dawniej klasy miejskiej) produkowany przez japoński koncern Honda Motor Company od 1972 roku jako następca modelu N360 oraz N600.',
+1,
+'https://pl.wikipedia.org/wiki/Honda_Civic#/media/File:CVC2012aaa.jpg',
+1);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Honda Accord ',
+50,
+'Honda Accord - samochód osobowy klasy średniej produkowany przez japoński koncern Honda Motor Company od 7 maja 1976 roku jako następca modelu 1300.',
+1,
+'https://pl.wikipedia.org/wiki/Honda_Accord#/media/File:Honda_Accord_2.4_EXL_2014_%2811879731796%29.jpg',
+1);
+
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Volvo S60 ',
+60,
+' samochód osobowy klasy średniej produkowany przez koncern Volvo Car Corporation jako sedan.',
+1,
+'https://pl.wikipedia.org/wiki/Volvo_S60#/media/File:Volvo_S60R_Forss.jpg',
+1);
+
+/* modele dostawcze */
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Ford Transit  ',
+110,
+' Samochód dostawczy marki Ford produkowany od 1965 roku występujący w różnych typach nadwozi od dostawczego aż po 17-osobowego busa.',
+1,
+'https://pl.wikipedia.org/wiki/Ford_Transit#/media/File:NA_Transit_2013.JPG',
+2);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Mercedes-Benz Sprinter',
+100,
+'Mercedes-Benz Sprinter − rodzina ciężkich samochodów dostawczych i lekkich samochodów ciężarowych o DMC do 5 ton produkowanych od 1995 roku.',
+1,
+'https://pl.wikipedia.org/wiki/Mercedes-Benz_Sprinter#/media/File:Mercedes-Benz_Sprinter_Mixto_216_CDI_%28W_906,_Facelift%29_%E2%80%93_Frontansicht,_8._September_2013,_B%C3%B6sensell.jpg',
+2);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Ford Transit Connect ',
+85,
+'Ford Transit Connect (Ford Tourneo Connect, Ford Transit Courier) – pojazd typu minivan przeznaczony do przewozu osób (Tourneo) oraz jako małe auto dostawcze (Transit) dla małych firm produkowany od 2002 roku.',
+1,
+'https://pl.wikipedia.org/wiki/Ford_Transit_Connect#/media/File:2013-03-05_Geneva_Motor_Show_8238.JPG',
+2);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Citroen Berlingo ',
+75,
+'Citroen Berlingo – samochód osobowo-dostawczy typu kombivan francuskiej marki Citroën produkowany od 1996 roku we współpracy z Peugeotem.',
+1,
+'https://pl.wikipedia.org/wiki/Citro%C3%ABn_Berlingo#/media/File:Citro%C3%ABn_Berlingo_Multispace_XTR_%28II,_Facelift%29_%E2%80%93_Frontansicht,_5._September_2012,_Wuppertal.jpg',
+2);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'VOLVO FM 9 ',
+200,
+'Volvo FM − gama samochodów ciężarowych produkowanych przez szwedzkie Volvo Truck Corporation.',
+1,
+'https://pl.wikipedia.org/wiki/Volvo_FM#/media/File:DHL-PN06CNO.jpg',
+3);
+
+INSERT INTO `s179647`.`Car`
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Scania 114L ',
+210,
+'Scania serii 4 − to wprowadzony do sprzedaży w 1996 roku następca serii 3. Zdobywczyni tytułu "International Truck of the Year" roku 1996.',
+1,
+'https://pl.wikipedia.org/wiki/Scania_serii_4#/media/File:Scania_114L-Heisterkamp_%28NL%29-2003.jpg',
+3);
+
+(`car_id`,
+`name`,
+`price`,
+`description`,
+`category_id`,
+`img_url`,
+`status`)
+VALUES
+('',
+'Mercedes-Benz Axor  ',
+210,
+'Mercedes-Benz Actros – samochód ciężarowy produkowany przez firmę Mercedes-Benz od 1996 - seria Mp1 (zdobywca tytułu "International Truck of the Year" 1997), którą produkowano do roku 2003.',
+1,
+'https://pl.wikipedia.org/wiki/Mercedes-Benz_Actros#/media/File:Actros_MP3.JPG',
+3);
