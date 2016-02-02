7498fad218a1d07c0f54846a00ed52fab4330f9c
Dominik Adamski <s179647@v-ie.uek.krakow.pl>
2016-02-02 13:23:43 +0100

    Zmiana pliku baza_danych.sql - dodanie 2 nowych tabel.

--- baza_danych/baza_danych.sql
+++ baza_danych/baza_danych.sql
@@ -1,13 +1,14 @@
-﻿CREATE TABLE User 
+CREATE TABLE User 
 (
 user_id int auto_increment,
 login varchar(20) NOT NULL,
 password varchar(30) NOT NULL,
 mail varchar(30) NOT NULL,
-telephon_number varchar(16) NOT NULL,
+telephone_number varchar(16) NOT NULL,
+role int2 NOT NULL,
+status int,
 PRIMARY KEY(user_id)
 );
-
 
 CREATE TABLE Category
 (
@@ -15,7 +16,6 @@ category_id int auto_increment,
 category varchar(20) NOT NULL,
 PRIMARY KEY(category_id)
 );
-
 
 CREATE TABLE Car 
 (
@@ -25,7 +25,8 @@ price int(6) NOT NULL,
 description varchar(256) NOT NULL,
 category_id int,
 img_url varchar(150),
-status int(1),
+seats varchar(10),
+status int,
 PRIMARY KEY(car_id),
 FOREIGN KEY (category_id) references Category(category_id)
 );
@@ -40,4 +41,23 @@ rate int,
 PRIMARY KEY(review_id),
 FOREIGN KEY(car_id) references Car (car_id),
 FOREIGN KEY(user_id) references User (user_id)
+);
+
+CREATE TABLE Order (
+order_id int auto_increment,
+user_id int NOT NULL,
+order_data date NOT NULL,
+order_status int NOT NULL,
+status int,
+PRIMARY KEY(order_id),
+FOREIGN KEY (user_id) references User (user_id)
+);
+
+CREATE TABLE Has_Order (
+order_has_car_id int auto_increment,
+order_id int NOT NULL,
+car_id int NOT NULL,
+PRIMARY KEY(order_has_car_id),
+FOREIGN KEY (car_id) references Car (car_id),
+FOREIGN KEY (order_id) references Order (order_id)
 );
